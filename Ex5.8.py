3ffaae7d28aeffeaf92f45d4851168a3ea4d215b
Victor <87349107+Vycttor@users.noreply.github.com>
2024-11-19 18:47:23 -0300

    Update Ex5.8.py

--- Ex5.8.py
+++ Ex5.8.py
@@ -1,3 +1,4 @@
 '''
 Ex 5.8. Escreva um programa que leia dois numeros. Imprima o resultado da multiplicação do primeiro pelo segundo. Utilize operadores de soma e subtração para calcular o resultado. Lembre se de que podemos entender a multiplicação de dois números como somas sucessivas de um deles. Assim , 4x5 = 5 + 5 + 5 + 5 = 4 + 4 + 4 + 4 + 4.
 '''
+
