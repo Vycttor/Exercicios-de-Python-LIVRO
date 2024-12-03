fb27d8c54cf5a9a7c4fe73fb6fa45befb7a86c2f
Victor <87349107+Vycttor@users.noreply.github.com>
2024-12-03 14:24:17 -0300

    aprendendo whi

--- Ex5.11.py
+++ Ex5.11.py
@@ -1,26 +1,33 @@
 '''
 Ex. Escreva um programa que pergunte o depósito inicial e a taxa de juros de uma poupança. Exiba os valores mês a mês para os 24 primeiros meses. Escreva o total ganho com juros no período.
 '''
-cont = 1
-soma = 0
-somaj = 0
-mes = 1
+cont = 0
+soma = somaj = 0
+mes = 0
+print('==='*20)
+print('========== POUPANÇA ==========')
+print('==='*20)
 
 deposito = float(input('Informe o valor de depósito:R$ '))
 juros_poupanca = float(input('Informe o taxa de juros:% '))
+print('==='*20)
+print('\n')
 
-while cont <= 10: 
-
+while cont < 24: 
     soma = soma + deposito
-
-    #somaj = soma + (soma*juros_poupanca) / 100
-    print(f'[{mes}]: R$ {soma:.2f}')
-    cont = cont + 1
     mes = mes + 1
-   
+    cont = cont + 1
+    print(f'[{mes}]: R$ {soma:.2f}')
+    if soma > 0:
+        juros = (soma*juros_poupanca) /100
+        #print(f'JUROS {juros}')
+        somaj = somaj + juros
+        #print(f'SOMAJ {somaj}')
+print('\n')
+print('==='*20)       
+print(f'Valor depósito 24 mêses: R$ {deposito:.2f} reais ')     
+print(f'Valor de juros da poupança {juros_poupanca} %')
+print(f'Valor juros dos 24 mêses é de R$ {somaj} reais')
+print(f'Total de rendimento dos 24 meses R$ {soma+somaj}')
+print('==='*20)
 
-print(f'Soma dos 24 meses {soma}')
-
-#pendente calcular juros mes a mes
-
-    
